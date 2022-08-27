ff3114035471e4e3737de88d40dddd9241f00ad7
WBomfim <willianbomfim@hotmail.com>
2022-08-27 20:07:57 -0300

    feat: Criado um middleware para lidar com erros não previstos nas rotas

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const handleErrors = require('./middlewares/handleErrors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(handleErrors);
 
 module.exports = app;
---
+++ src/middlewares/handleErrors.js
@@ -0,0 +1,4 @@
+const handleErrors = (_err, _req, res, _next) => res.status(500)
+  .json({ message: 'Internal server error' });
+
+module.exports = handleErrors;
