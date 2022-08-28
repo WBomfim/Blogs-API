ffc7831670f6e5d28bbae831cc4eb6e74876da6c
WBomfim <willianbomfim@hotmail.com>
2022-08-27 21:21:00 -0300

    feat: Criado o service login para validar o login do usuário e retornar um token em caso de sucesso

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,25 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { User } = require('../database/models');
+const validateUserInfos = require('../schemas/validateUserInfos');
 
 const login = ({ email, password }) => {
+  const isNotValidInfos = validateUserInfos({ email, password });
+  if (isNotValidInfos) return isNotValidInfos;
 
+  const user = User.findOne({ where: { email } });
+  if (!user || !user.password === password) {
+    return { code: 400, error: { message: 'Invalid fields' } };
+  }
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+  return { code: 200, data: { token } };
 };
 
 module.exports = {
