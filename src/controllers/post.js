10ee556ab9f74b95992795c42eff72abc1a7dd3c
WBomfim <willianbomfim@hotmail.com>
2022-08-28 19:58:26 -0300

    fix: Corrigido a sintaxe do controller addPost

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,8 +1,9 @@
 const postService = require('../services/post');
 
-const addPost = async ({ title, content, categoryIds }) => {
-  const newPost = await postService.addPost({ title, content, categoryIds });
-  return { code: 201, data: newPost };
+const addPost = async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { code, data } = await postService.addPost({ title, content, categoryIds });
+  return res.status(code).json(data);
 };
 
 module.exports = {
