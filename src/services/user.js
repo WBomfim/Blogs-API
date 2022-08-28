76e6bdcb1df2324a4542d3ae8cb34b4c1bb3ae63
WBomfim <willianbomfim@hotmail.com>
2022-08-28 16:25:35 -0300

    refactor: Auterado o service user desestruturando o retorno do schema validateUserInfos para melhorar a semântica

--- src/services/user.js
+++ src/services/user.js
@@ -3,8 +3,8 @@ const validateUserInfos = require('../schemas/validateUserInfos');
 const generateToken = require('../helpers/generateToken');
 
 const addUser = async ({ displayName, email, password, image }) => {
-  const validateInfos = await validateUserInfos({ displayName, email, password });
-  if (validateInfos.error) return validateInfos.error;
+  const { error } = await validateUserInfos({ displayName, email, password });
+  if (error) return error;
 
   const newUser = await User.create({ displayName, email, password, image });
   const token = generateToken(newUser);
