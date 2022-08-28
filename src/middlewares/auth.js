de02d6c03c63f92320f552cfb78c3eef1de2fd08
WBomfim <willianbomfim@hotmail.com>
2022-08-28 14:28:27 -0300

    feat: Criado o middleware auth para realizar a autenticação dos usuários ao acesso das rotas.

---
+++ src/middlewares/auth.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header.authotization;
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const verified = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = verified;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = auth;
