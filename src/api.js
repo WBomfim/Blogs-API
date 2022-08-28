83c88785321d64e4cb6013e93f89d7133b9fcc7d
WBomfim <willianbomfim@hotmail.com>
2022-08-27 21:38:00 -0300

    feat: Criado o endpoint POST /login

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const handleErrors = require('./middlewares/handleErrors');
+const loginRouter = require('./routes/login');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRouter);
 
 app.use(handleErrors);
 
